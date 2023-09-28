84018f5f4dabb434b3efa21bedde27c53d9fcada
ashishyadav2k2 <unixm98@gmail.com>
2023-09-28 21:58:08 +0530

    add final ml model

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ df = pkl.load(open('G:\Ashish Yadav\Backup Google Drive\Documents-Lecture\Github
 
 mapping = pkl.load(open('G:\Ashish Yadav\Backup Google Drive\Documents-Lecture\Github\ML-Mini-Project\config\car_model_mapping.pkl','rb'))
 
-# ['Make','Model','Year','Kilometer','Fuel Type','Transmission','Owner','Engine', 'Length','Seating Capacity', 'Fuel Tank Capacity','Price']
+# ['Make','Model','Year', 'Kilometer', 'Fuel Type','Transmission', 'Location', 'Color', 'Owner','Engine','Drivetrain','Price']
 locale.setlocale(locale.LC_ALL, 'en_IN')
 st.title('Car Price Prediction')
 company = st.selectbox('Manufacturer',sorted(mapping.keys()))
@@ -25,20 +25,20 @@ year = st.number_input('Purchase Year',min_value=1930,max_value=datetime.date.to
 km = st.number_input('Kilometers Driven',step=1000)
 fuel_type = st.selectbox('Fuel Type',df['Fuel Type'].unique())
 transmission = st.selectbox('Transmission Type',df['Transmission'].unique())
+location = st.selectbox('Location',sorted(df['Location'].unique()))
+color = st.selectbox('Color',sorted(df['Color'].unique()))
 ownership = st.selectbox('Ownership Type',df['Owner'].unique())
 engine_size = st.number_input('Engine Size',step=50)
-length = st.number_input('Length in meters',step=0.5)*1000
-seat_capacity = st.number_input('Seating Capacity',step=1)
-fuel_capacity = st.number_input('Fuel Tank Capacity',step=5)
+drive_train = st.selectbox('Location',sorted(df['Drivetrain'].unique()))
 
 if st.button('Predict Price'):
     with st.spinner('Thinking'):
         time.sleep(5)
         
-    query = np.array([[company,car_model,year,km,fuel_type,transmission,ownership,engine_size,length,seat_capacity,fuel_capacity]])
+    query = np.array([[company,car_model,year,km,fuel_type,transmission,location,color,ownership,engine_size,drive_train]])
     prediction = model.predict(query)[0]
-    lower_bound = prediction - ((prediction*8)/100)
-    upper_bound = prediction + ((prediction*8)/100)
+    lower_bound = prediction - (prediction*0.09)
+    upper_bound = prediction + (prediction*0.09)
     f1 = locale.currency(lower_bound, grouping=True)
     f2 = locale.currency(upper_bound, grouping=True)
     avg = locale.currency(prediction, grouping=True)
